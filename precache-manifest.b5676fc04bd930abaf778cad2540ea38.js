429271456cb85a6a1794807d3a8183238b1bf16f
Quardiz <quardiz@protonmail.com>
2020-08-31 16:52:30 +0200

    Updates

---
+++ precache-manifest.b5676fc04bd930abaf778cad2540ea38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c2ebca651cc7732033af1260d9caff",
+    "url": "/DopingScatterPlot/index.html"
+  },
+  {
+    "revision": "2c52727a8f1f158d98e2",
+    "url": "/DopingScatterPlot/static/css/main.7e622ad0.chunk.css"
+  },
+  {
+    "revision": "21134d00d735ee8ae09f",
+    "url": "/DopingScatterPlot/static/js/2.0426cd3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DopingScatterPlot/static/js/2.0426cd3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c52727a8f1f158d98e2",
+    "url": "/DopingScatterPlot/static/js/main.9e1ab9d5.chunk.js"
+  },
+  {
+    "revision": "0ab5ea27bb7ae64444fa",
+    "url": "/DopingScatterPlot/static/js/runtime-main.5eb78f80.js"
+  }
+]);
